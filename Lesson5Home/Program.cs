6a4f107126a8003035279e770e52b73446b2fb58
Эмир <@DESKTOP-AI8L0VK>
2024-01-21 16:09:34 +0300

    Добавьте файлы проекта.

---
+++ Lesson5Home/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("а) Используя константы, напишете программу для перевода милимметров в сантиметры:");
+const double cm = 10;
+Console.WriteLine(cm);
+Console.WriteLine("\n");
+
+Console.WriteLine("b) Используя константы, напишете программу для перевода милимметров в метры:");
+const double m = cm*100;
+Console.WriteLine(m);
+Console.WriteLine("\n");
+
+Console.WriteLine("c) Используя константы, напишете программу для перевода милимметров в километры:");
+const double km = m * 1000;
+Console.WriteLine(km);
+Console.WriteLine("\n");
+
+Console.WriteLine("d) Используя константы, напишете программу для перевода милимметров в мили:");
+const double ml = km * 1.6;
+Console.WriteLine(ml);
+Console.WriteLine("\n");
+
+Console.WriteLine("e) Используя константы, напишете программу для перевода милимметров в километры в ярды:");
+const double yd = m * 0.9;
+Console.WriteLine(yd);
+
+
+
